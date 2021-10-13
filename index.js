6de61a8517a9ac2819b558abcc649c6991a51d57
AlexRazor1337 <alexrazor1337@gmail.com>
2021-10-13 23:34:54 +0300

    Fix regexp, add classmap and template loading

--- index.js
+++ index.js
@@ -3,21 +3,13 @@ showdown.setOption('strikethrough', true);
 const fs = require('fs')
 const showdownHighlight = require('showdown-highlight');
 
-const classMap = {
-    h1: 'header'
-}
-
+const classMap = fs.existsSync('classmap.json') ? JSON.parse(fs.readFileSync('classmap.json').toString()) : new Object();
 
 const bindings = Object.keys(classMap).map(key => ({
     type: 'output',
-    regex: new RegExp(`<${key}(.*)>`, 'g'),
-    replace: `<${key} class="${classMap[key]}" $1>`
+    regex: new RegExp(`<${key}\s*`, 'g'),
+    replace: `<${key} class="${classMap[key]}" $1`
 }));
-
-
-const appendString = `
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/styles/default.min.css">
-`
 
 
 const convertFile = async (filename) => {
@@ -30,8 +22,10 @@ const convertFile = async (filename) => {
         const converter = new showdown.Converter({
             extensions: [...bindings, showdownHighlight({ pre: true })]
         });
+
         const html = converter.makeHtml(data.toString());
-        fs.writeFile(filename.replace('.md', '') + '.html', appendString + html, { flag: 'w+' }, err => {})
+        const template = fs.existsSync('template.html') ? fs.readFileSync('template.html').toString() : '<!DOCTYPE html><html lang="en"><body>[CONTENT GOES HERE]</body></html>';
+        fs.writeFile(filename.replace('.md', '') + '.html', template.replace('[CONTENT GOES HERE]', html), { flag: 'w+' }, err => {})
     });
 }
 
